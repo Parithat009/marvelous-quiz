c679a9d888a4c5ce2cc6f0d828f59bc348b68d0b
nut-parithat <starsis25391@gmail.com>
2021-05-05 17:48:49 +0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import { createRenderer } from 'fela'
+import { RendererProvider } from 'react-fela'
 import '../styles/globals.css'
 import { Context } from '../hook/context'
 
+const renderer = createRenderer()
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Context>
-      <Component {...pageProps} />
-    </Context>
+    <RendererProvider renderer={renderer}>
+      <Context>
+        <Component {...pageProps} />
+      </Context>
+    </RendererProvider>
   )
 }
 
---
+++ pages/felafela.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useFela } from 'react-fela'
+
+const Felafela = () => {
+  const styles = useStyleSheet()
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Content}>
+        Felafela
+      </div>
+    </div>
+  )
+}
+
+const useStyleSheet = () => {
+  const { css } = useFela()
+
+  return {
+    Container: css({
+      display: 'flex',
+      flexDirection: 'row',
+      width: '100%',
+      backgroundColor: 'green'
+    }),
+    Content: css({
+      maxWidth: '320px',
+      width: '100%',
+      margin: '1rem auto',
+      textAlign: 'center',
+      backgroundColor: 'greenyellow'
+    })
+  }
+}
+
+export default Felafela
